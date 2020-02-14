72a372a2028e13ab60b680caaca60dcc07426830
Rainier Laan <rainier.laan@home.nl>
2020-02-14 15:50:31 +0100

    RLCMS-3

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -17,7 +17,7 @@ class MessageController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index()
     {
@@ -108,11 +108,15 @@ class MessageController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function destroy($id)
     {
-        //
+        $message = Message::find($id);
+
+        $message->delete();
+
+        return back();
     }
 
     /**
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -17,36 +17,36 @@
         </div>
     </div>
 
-{{--    <div class="row my-5">--}}
-{{--        <div class="col-md-12">--}}
-{{--            <form action="{{ route('create_message') }}" method="POST">--}}
-{{--                @csrf--}}
-{{--                <div class="form-grouo row">--}}
-{{--                    <div class="form-group col-6">--}}
-{{--                        <label for="firstname">{{ __('Firstname') }}</label>--}}
-{{--                        <input type="text" name="firstname" class="form-control" id="firstname">--}}
-{{--                    </div>--}}
-{{--                    <div class="form-group col-6">--}}
-{{--                        <label for="lastname">{{ __('Lastname') }}</label>--}}
-{{--                        <input type="text" name="lastname" class="form-control" id="lastname">--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="form-group">--}}
-{{--                    <label for="email">{{ __('Email') }}</label>--}}
-{{--                    <input type="email" name="email" class="form-control" id="email">--}}
-{{--                </div>--}}
-{{--                <div class="form-group">--}}
-{{--                    <label for="title">{{ __('Title') }}</label>--}}
-{{--                    <input type="text" name="title" class="form-control" id="title">--}}
-{{--                </div>--}}
-{{--                <div class="form-group">--}}
-{{--                    <label for="message">{{ __('Message') }}</label>--}}
-{{--                    <textarea name="message" id="message" class="form-control" cols="30" rows="10"></textarea>--}}
-{{--                </div>--}}
-{{--                <div class="form-group">--}}
-{{--                    <button type="submit" class="btn btn-primary">{{ __('Send form') }}</button>--}}
-{{--                </div>--}}
-{{--            </form>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+    <div class="row my-5">
+        <div class="col-md-12">
+            <form action="{{ route('create_message') }}" method="POST">
+                @csrf
+                <div class="form-grouo row">
+                    <div class="form-group col-6">
+                        <label for="firstname">{{ __('Firstname') }}</label>
+                        <input type="text" name="firstname" class="form-control" id="firstname">
+                    </div>
+                    <div class="form-group col-6">
+                        <label for="lastname">{{ __('Lastname') }}</label>
+                        <input type="text" name="lastname" class="form-control" id="lastname">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="email">{{ __('Email') }}</label>
+                    <input type="email" name="email" class="form-control" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="title">{{ __('Title') }}</label>
+                    <input type="text" name="title" class="form-control" id="title">
+                </div>
+                <div class="form-group">
+                    <label for="message">{{ __('Message') }}</label>
+                    <textarea name="message" id="message" class="form-control" cols="30" rows="10"></textarea>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">{{ __('Send form') }}</button>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
